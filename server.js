afdad8b6bdb30c111339e487bc997e4949f5ca73
sebli <seblewongelabera@gmail.com>
2022-01-17 15:52:37 -0800

    added routes, established DB connection added express function to parse the body and cookier

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const colors = require('colors');
 require('dotenv').config();
+const dbConnection = require('./config/db');
+// Load routes
+const auth = require('./routes/auth');
+const cookieParser = require('cookie-parser');
 
+// DB connection
+dbConnection();
 const app = express();
+// parse the body
+app.use(express.json());
+// parse the cookier
+app.use(cookieParser());
 
+app.use('/api/v1/auth', auth);
 app.use('/api/v1/', (req, res) => {
   res.send({ message: 'Hi' });
 });
@@ -10,5 +22,5 @@ app.use('/api/v1/', (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} on Port ${PORT}`);
+  console.log(`Server running in ${process.env.NODE_ENV} on Port ${PORT}`.yellow.bold);
 });
