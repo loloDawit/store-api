0c4e5ed3c8bc8c1392a07ca572de8c624a6f4be7
dawit.abera <dawit.y.abera@nordstrom.com>
2022-01-17 18:38:07 -0800

    handle promise rejections

--- server.js
+++ server.js
@@ -25,4 +25,13 @@ const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} on Port ${PORT}`.yellow.bold);
 });
 
+// Handle all promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  logger.error(`Error: ${err.message}`.red.bold);
+  // close server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 module.exports = server;
