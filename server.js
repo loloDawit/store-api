7e56419a0cea4d362590d9710d185dd81cf2c4b8
dawit.abera <dawit.y.abera@nordstrom.com>
2022-01-17 18:48:46 -0800

    - updated server to more secure and install packages

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
 const colors = require('colors');
-require('dotenv').config();
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const xss = require('xss-clean');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const dbConnection = require('./config/db');
+require('dotenv').config();
 // Load routes
 const auth = require('./routes/auth');
-const cookieParser = require('cookie-parser');
 
 // DB connection
 dbConnection();
 const app = express();
-// parse the body
+
+// Body parser
 app.use(express.json());
-// parse the cookier
+// Cookie parser
 app.use(cookieParser());
+
+// DB protection:
+app.use(mongoSanitize());
+
+// Security features
+app.use(helmet());
+
+// More security and remove scripts
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100
+});
+app.use(limiter);
+
+// prevent hpp param pollution
+app.use(hpp());
+// Enable CORS
+app.use(cors());
 
 // logs HTTP responses
 if (process.env.NODE_ENV === 'development') {
@@ -31,7 +61,7 @@ const server = app.listen(PORT, () => {
 
 // Handle all promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  logger.error(`Error: ${err.message}`.red.bold);
+  console.error(`Error: ${err.message}`.red.bold);
   // close server and exit process
   server.close(() => {
     process.exit(1);
