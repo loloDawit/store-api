53e0b1445cd08c90686d32c44e4a2ecc9775194a
sebli <seblewongelabera@gmail.com>
2022-01-17 16:01:35 -0800

    updated mistyping

--- models/Users.js
+++ models/Users.js
@@ -16,24 +16,24 @@ const UserSchema = new mongoose.Schema({
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       'Please use a valid email',
     ],
-    password: {
-      type: String,
-      required: [true, 'Please add a password'],
-      minlength: 8,
-      select: false,
-    },
-    role: {
-      type: String,
-      enum: ['user', 'admin'],
-      default: 'user',
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
   },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 8,
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
 
 // Encrypt user password
