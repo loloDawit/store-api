a7ff163daf96ffc27732d22eb63aa6f10c7f0bac
dawit.abera <dawit.y.abera@nordstrom.com>
2022-01-18 22:10:24 -0800

    create signin route

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { registerUser } = require('../controllers/users');
+const { registerUser, signInUser } = require('../controllers/users');
 
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/signin', signInUser);
 
 module.exports = router;
