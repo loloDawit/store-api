919291c5460e7ba3aa60cba525eb1857732cdbc7
dawit.abera <dawit.y.abera@nordstrom.com>
2022-01-18 22:09:57 -0800

    add function to validate password

--- models/Users.js
+++ models/Users.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please add a name'],
-    maxlength: [15, 'Name can not be more than 15 char'],
+    maxlength: [15, 'Name can not be more than 15 char']
   },
   email: {
     type: String,
@@ -14,26 +14,26 @@ const UserSchema = new mongoose.Schema({
     unique: true,
     match: [
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please use a valid email',
-    ],
+      'Please use a valid email'
+    ]
   },
   password: {
     type: String,
     required: [true, 'Please add a password'],
     minlength: 8,
-    select: false,
+    select: false
   },
   role: {
     type: String,
     enum: ['user', 'admin'],
-    default: 'user',
+    default: 'user'
   },
   createdAt: {
     type: Date,
-    default: Date.now(),
+    default: Date.now()
   },
   resetPasswordToken: String,
-  resetPasswordExpire: Date,
+  resetPasswordExpire: Date
 });
 
 // Encrypt user password
@@ -44,10 +44,18 @@ UserSchema.pre('save', async function (next) {
   const salt = await bcryptjs.genSalt(10);
   this.password = await bcryptjs.hash(this.password, salt);
 });
+/**
+ * Match user's password saved in the database which is already hashed
+ * @param {*} password
+ * @returns boolean
+ */
+UserSchema.methods.validateHashedPassword = async function (password) {
+  return await bcryptjs.compare(password, this.password);
+};
 
 UserSchema.methods.getSignedJWT = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE_DATE,
+    expiresIn: process.env.JWT_EXPIRE_DATE
   });
 };
 
