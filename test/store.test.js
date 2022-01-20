1856b49b07496939401569fd7db6e5be7f99e385
sebli <seblewongelabera@gmail.com>
2022-01-19 19:31:42 -0800

    test code for register

--- test/store.test.js
+++ test/store.test.js
@@ -3,6 +3,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { expect } = require('chai');
 
+
 const should = chai.should();
 
 chai.use(chaiHttp);
@@ -14,6 +15,18 @@ describe('/ check index', () => {
       .get('/api/v1/')
       .end((err, res) => {
         res.should.have.status(200);
+        done();
+      });
+  });
+});
+describe('/POST ', () => {
+  it('it should return 201', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/auth/register')
+      .send(token)
+      .end((err, res) => {
+        res.should.have.status(201);
         done();
       });
   });
