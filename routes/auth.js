e57e327114a67bfa89b32d00d2abb47b748fe408
dawit.abera <dawit.y.abera@nordstrom.com>
2022-01-29 17:33:30 -0800

    add update route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
-const { registerUser, signInUser } = require('../controllers/users');
+const { registerUser, signInUser, updateUserDetails } = require('../controllers/users');
+const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/signin', signInUser);
+router.put('/update', protect, updateUserDetails);
 
 module.exports = router;
