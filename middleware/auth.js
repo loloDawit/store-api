55a04062bf0795ee5a5c4d93a0131b118f528b89
dawit.abera <dawit.y.abera@nordstrom.com>
2022-01-30 11:58:54 -0800

    update protect function

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
   let token;
   // check token in headers or cookies
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    token = req.headers.authorization;
+    token = req.headers.authorization.split(' ')[1];
   } else if (req.cookies.token) {
     token = req.cookies.token;
   }
@@ -18,8 +18,11 @@ exports.protect = asyncHandler(async (req, res, next) => {
   try {
     const verifyToken = jsonWebToken.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(verifyToken.id);
+    if (!req.user) {
+      return next(new ErrorResponse('user not found', 400));
+    }
     next();
   } catch (error) {
-    return next(new ErrorResponse('Not authorized to access this route.', 401));
+    return next(new ErrorResponse(error, 401));
   }
 });
