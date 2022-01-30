91a11ab9eaf98242ccf78956f06bd518cd3f1b6e
dawit.abera <dawit.y.abera@nordstrom.com>
2022-01-29 17:31:27 -0800

    update error response

--- utils/error.js
+++ utils/error.js
@@ -1,8 +1,9 @@
 class ErrorResponse extends Error {
-  constructor(error) {
+  constructor(error, statusCode) {
     super();
     this.message = error?.message || error;
     this.error = error?._message || error;
+    this.statusCode = statusCode;
   }
 }
 module.exports = ErrorResponse;
