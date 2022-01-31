8563477ee07450daacb0c82beab324a0efebb3ec
dawit.abera <dawit.y.abera@nordstrom.com>
2022-01-30 18:34:23 -0800

    add resetpassword routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
-const { registerUser, signInUser, updateUserDetails, updateUserPassword } = require('../controllers/users');
+const {
+  registerUser,
+  signInUser,
+  updateUserDetails,
+  updateUserPassword,
+  resetPasswordRequest,
+  resetPassword
+} = require('../controllers/users');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
@@ -9,5 +16,7 @@ router.post('/register', registerUser);
 router.post('/signin', signInUser);
 router.put('/update', protect, updateUserDetails);
 router.put('/updatepassword', protect, updateUserPassword);
+router.post('/resetpassword', resetPasswordRequest);
+router.put('/resetpassword/:token', resetPassword);
 
 module.exports = router;
