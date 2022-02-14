6e3e9eb58efe42f371cff64f078edf04fe7c51fd
sebli <seblewongelabera@gmail.com>
2022-02-14 10:08:26 -0800

    updated test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-
 const address = {
+  
   storeNumber: '201',
   id: '',
-  firstName: 'dawitdawitdawitdawitdawitdawitdawixdawit',
+  firstName: 'dawitdawitdawitdawitdawitdawitdawixdawitoooop',
   middleName: 'azziidawitdawitdawitdawitdawitdawixdawit',
   lastName: 'Abera',
   addressLine1: '13 7th Ave',
@@ -12,57 +12,56 @@ const address = {
   postalCode: '10011',
   countryCode: 'US',
   default: false,
-  saveAsDefault: false,
-  
+  saveAsDefault: false
 }
-
+;
 
 // problem: address fields longer than 35char are breaking downstream servcies
 // An an engineer, i want to make sure, these fields are less or equal to 35chars
 // create a function that takes in address and validate the values.
 
-// x is obj {}
-// key : value
-// key y,z
-// value 1,2
-// var array [1,2]
 
-var x = {
-  y: 1,
-  z: 2
-};
-// how can i check if a given string is 35 or not
-var b = 'dawitdawitdawitdawitdawitdawitdawixdawit';
-// console.log(b.substring(0, 35));
+const validater = (address) => {
+  
+  if (address) {
+    if (address.storeNumber.length > 35) {
+      address.storeNumber.substring(0, 35);
+    }
+    if (address.id.length > 35) {
+      address.id.substring(0, 35);
+    }
+    if (address.firstName.length > 35) {
+      address.firstName.substring(0, 35);
+    }
+    if (address.middleName.length > 35) {
+      address.middleName.substring(0, 35);
+    }
+    if (address.lastName.length > 35) {
+      address.lastName.substring(0, 35);
+    }
+    if (address.addressLine1.length > 35) {
+      address.addressLine1.substring(0, 35);
+    }
+    if (address.addressLine2.length > 35) {
+      address.addressLine2.substring(0, 35);
+    }
+    if (address.city.length > 35) {
+      address.city.substring(0, 35);
+    }
+    if (address.state.length > 35) {
+      address.state.substring(0, 35);
+    }
+    if (address.postalCode.length > 35) {
+      address.postalCode.substring(0, 35);
+    }
+    if (address.countryCode.length > 35) {
+      address.countryCode.substring(0, 35);
+    }
 
-var p = [
-  '201',
-  '',
-  'dawitdawitdawitdawitdawitdawitdawixdawit',
-  'azziidawitdawitdawitdawitdawitdawixdawit',
-  'Abera',
-  '13 7th Ave',
-  '',
-  'New York',
-  'NY',
-  '10011',
-  'US',
-  false,
-  false
-];
-
-
-let newValues = ''
-p.forEach((v) => {
-
-  if (v.length > 35) {
-    newValues += v.substring(0,35) + '\n'
-  }else{
-    newValues += v + '\n'
+    return address;
   }
-  return newValues
-});
-
-console.log(newValues);
+};
 
 
+const x = validater(address)
+console.log(x.firstName);
